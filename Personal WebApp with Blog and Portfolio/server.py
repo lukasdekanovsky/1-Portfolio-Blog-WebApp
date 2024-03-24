a8421a8a8d885bdfa555c8fd4309068225151f49
Lukáš Děkanovský <lukasdekanovsky@gmail.com>
2024-03-24 23:38:37 +0100

    v1.01

--- Personal WebApp with Blog and Portfolio/server.py
+++ Personal WebApp with Blog and Portfolio/server.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/base")
+def base():
+    return render_template("base.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
