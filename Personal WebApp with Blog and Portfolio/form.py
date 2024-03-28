7651f89083251ba5d53287e86fedd7dfa4efa4db
Lukáš Děkanovský <lukasdekanovsky@gmail.com>
2024-03-28 23:15:27 +0100

    v1.10_all logic implemented

--- Personal WebApp with Blog and Portfolio/form.py
+++ Personal WebApp with Blog and Portfolio/form.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed, FileRequired
-from wtforms import StringField, SubmitField, IntegerField, URLField, FloatField, validators
-from wtforms.validators import DataRequired
+from wtforms import StringField,TextAreaField, SubmitField, IntegerField, URLField, FloatField, validators
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditorField
 
 class AddProject(FlaskForm):
     intro_title = StringField(label="Enter the main project title", validators=[validators.DataRequired()])
@@ -22,3 +23,28 @@ class EditProject(FlaskForm):
     new_image = FileField(label="Upload project image")
     new_gitlink = URLField(label="Enter github repo link")
     submit = SubmitField(label="Submit changes")
+
+
+# ----------------------------------------------------------------------------------------------------------
+
+class RegisterForm(FlaskForm):
+    email = StringField(label="Email", validators=[validators.DataRequired()])
+    password = StringField(label="Password", validators=[validators.DataRequired()])
+    name = StringField(label="Name", validators=[validators.DataRequired()])
+    submit = SubmitField(label="Sign me up!")
+
+class LoginForm(FlaskForm):
+    email = StringField(label="Email", validators=[validators.DataRequired()])
+    password = StringField(label="Password", validators=[validators.DataRequired()])
+    submit = SubmitField(label="Log in!")
+
+class CreatePostForm(FlaskForm):
+    title = StringField(label="Title", validators=[validators.data_required()])
+    subtitle = StringField(label="Subtitle", validators=[validators.DataRequired()])
+    img_url = StringField(label="Image URL", validators=[validators.DataRequired(), validators.URL()])
+    body = TextAreaField(label="Blog main body", validators=[validators.DataRequired()])
+    submit = SubmitField(label="Submit your post")
+
+class CommentForm(FlaskForm):
+    comment_text = TextAreaField("", validators=[DataRequired()])
+    submit = SubmitField("Submit Comment")
